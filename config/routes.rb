06d81e3aed28c336962be175ed4811f7e695e1dd
Genta <8581682+ga-ladder@users.noreply.github.com>
2018-10-04 14:45:48 +0900

    others

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'articles' => 'articles#index'
   get 'show' => 'articles#show'
+  get 'organizations' => 'organizations#index'
+  get 'organizations/show' => 'organizations#show'
 end
