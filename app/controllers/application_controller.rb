733f4af4dc73afe523f93accc1a6df4ee2476231
Genta <8581682+ga-ladder@users.noreply.github.com>
2018-10-15 11:46:39 +0900

    add devise function add ログイン機能 メールアドレスかユーザー名でログインできるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+def after_sign_out_path_for(resource)
+  "/users/sign_in"
+end
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:name, :email, :password)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'articles#index'
   get 'articles' => 'articles#index'
   get 'show' => 'articles#show'
   get 'organizations' => 'organizations#index'
